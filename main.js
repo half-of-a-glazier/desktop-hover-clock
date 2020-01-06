b2b793b9c298ac6dd95cd80bb5b919d1a0b749fb
Mirel Springer <mirel.springer@benmanage.com>
2020-01-06 13:05:07 +0200

    Set default start position

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ const path = require('path');
 const { ipcMain } = require('electron');
 var ipc = require('electron').ipcMain;
 let nodeConsole = require('console');
+const appFolder = path.dirname(process.execPath);
+const updateExe = path.resolve(appFolder, '..', 'Update.exe');
+const exeName = path.basename(process.execPath);
 
 let myConsole = new nodeConsole.Console(process.stdout, process.stderr);
 myConsole.log('It works!');
@@ -15,16 +18,31 @@ ipc.on('invokeAction', function(event, data) {
 	event.sender.send('actionReply', result);
 });
 
+app.setLoginItemSettings({
+	openAtLogin: true,
+	path: updateExe,
+	args: [
+	'--processStart', `"${exeName}"`,
+	'--process-start-args', `"--hidden"`
+	]
+});
+
 function createWindow() {
+	let { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
 	let win = new BrowserWindow({
 		width: 290,
 		height: 145,
+		x: width - 300,
+		y: height - 1020,
 		transparent: true,
 		frame: false,
+		skipTaskbar: true,
 		webPreferences: {
 			nodeIntegration: true
 		}
 	});
+
+	win.setResizable(false);
 	win.loadFile('index.html');
 
 	win.on('closed', () => {
